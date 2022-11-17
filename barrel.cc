8df5100c50c1e2969a73bf17b3012e1f06e30aea
Petr Stepanov <stepanovps@gmail.com>
2022-11-17 13:21:35 -0800

    Add MT

--- barrel.cc
+++ barrel.cc
@@ -1,39 +1,3 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-/// \file persistency/gdml/G01/load_gdml.cc
-/// \brief Main program of the persistency/gdml/G01 example
-//
-//
-//
-//
-// --------------------------------------------------------------
-//      GEANT 4 - load_gdml
-//
-// --------------------------------------------------------------
-
 #include "ActionInitialization.hh"
 #include "DetectorConstruction.hh"
 #include "PrimaryGeneratorAction.hh"
@@ -42,6 +6,7 @@
 #include <G4RunManagerFactory.hh>
 #include <G4UImanager.hh>
 #include <FTFP_BERT.hh>
+#include <QBBC.hh>
 #include <G4VisExecutive.hh>
 #include <G4UIExecutive.hh>
 #include <G4GDMLParser.hh>
@@ -57,22 +22,25 @@ int main(int argc, char **argv) {
   }
 
   G4GDMLParser* parser = new G4GDMLParser();
-
   // Uncomment below to avoid names stripping
   // parser->SetStripFlag(false);
 
   parser->SetOverlapCheck(true);
   parser->Read(argv[1]);
 
-  auto* runManager = G4RunManagerFactory::CreateRunManager(G4RunManagerType::SerialOnly);
+  // auto* runManager = G4RunManagerFactory::CreateRunManager(G4RunManagerType::SerialOnly);
+  G4RunManager* runManager = G4RunManagerFactory::CreateRunManager();
 
   runManager->SetUserInitialization(new DetectorConstruction(parser));
-  runManager->SetUserInitialization(new FTFP_BERT());
+  // G4VModularPhysicsList* physicsList = new FTFP_BERT();
+  G4VModularPhysicsList* physicsList = new QBBC();
+  physicsList->SetVerboseLevel(1);
+  runManager->SetUserInitialization(physicsList);
   runManager->SetUserInitialization(new ActionInitialization());
   runManager->Initialize();
 
   // Initialize visualization
-  G4VisManager *visManager = new G4VisExecutive;
+  G4VisManager *visManager = new G4VisExecutive();
   visManager->Initialize();
 
   // Get the pointer to the User Interface manager
