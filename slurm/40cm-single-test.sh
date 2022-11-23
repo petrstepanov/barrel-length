a6288d915d4ac13ffec0fdaa6c0868bb5bc92da5
Petr Stepanov <stepanovps@gmail.com>
2022-11-23 11:25:37 -0800

    Emission point positioning improvements

---
+++ slurm/40cm-single-test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --partition=priority
+#SBATCH --account=eic
+#SBATCH --mem-per-cpu=512
+#SBATCH --time=12:00:00
+#SBATCH --output=eres_40cm.out
+#SBATCH --error=eres_40cm.err
+
+# Pass macros to executable
+./barrel ./gdml/40cm/mother-single.gdml ./macros/batch-test.mac
