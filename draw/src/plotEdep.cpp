f818b9dfdc7ac6ede4d47d1a35eaccd7d38ceec8
Petr Stepanov <stepanovps@gmail.com>
2022-11-26 17:59:49 -0800

    Resolution plotting updates

--- draw/src/plotEdep.cpp
+++ draw/src/plotEdep.cpp
@@ -21,16 +21,17 @@ int plotEdep() {
   // Iterate through files and find maximum nPE branch value across all files
   // This is for the light yield plots for each light guide length to have uniform scale along X axis
   Double_t xAxisMax = FileUtils::getBranchMaximumInFiles(files, "barrel", "edep");
+  Double_t xAxisMin = FileUtils::getBranchMinimumInFiles(files, "barrel", "edep")*6;
 
   // Create canvas for plotting
   TCanvas* canvas = new TCanvas();
-  canvas->SetWindowSize(1680, 500);
-  canvas->Divide(3, 1, 1e-5, 1e-5);
+  canvas->SetWindowSize(files->GetSize()*560, 500);
+  canvas->Divide(files->GetSize(), 1, 1e-5, 1e-5);
 
   // Iterate through files, plot and fir energy resolutions
   for (TObject *o : *files) {
     static Int_t counter = 0;
-    TVirtualPad* pad = canvas->cd(counter++ + 1);
+    TVirtualPad* pad = canvas->cd(counter + 1);
     pad->SetGrid();
 
     // Get file name path
@@ -47,7 +48,7 @@ int plotEdep() {
     TString histName = TString::Format("hist-%s", filePath.name.Data());
     TString histTitle = "";
     // Int_t maxPE = FileUtils::getBranchMaximum(tree, "nPE")*0.3;
-    TH1 *hist = new TH1I(histName.Data(), histTitle.Data(), 150, 2000, xAxisMax);
+    TH1 *hist = new TH1I(histName.Data(), histTitle.Data(), 200, xAxisMin, xAxisMax);
     hist->GetXaxis()->SetTitle("Deposited energy per Event, MeV");
     TString title = TString::Format("Counts / %.2f MeV", hist->GetBinWidth(1));
     hist->GetYaxis()->SetTitle(title.Data());
@@ -67,6 +68,12 @@ int plotEdep() {
     gStyle->SetOptFit(111);
     hist->Fit(fBall);
 
+    // Plot gaussian part
+    TF1* fBallGauss = FitUtils::getCrystalBallGaussFunction(fBall);
+    fBallGauss->SetLineColor(kBlack);
+    fBallGauss->SetLineStyle(ELineStyle::kDashed);
+    fBallGauss->Draw("SAME");
+
     // Get fitting parameters
     Double_t m = hist->GetFunction("fBall")->GetParameter(2); // mean
     Double_t Dm = hist->GetFunction("fBall")->GetParError(2); // mean error
@@ -75,8 +82,13 @@ int plotEdep() {
 
     // GAUSS RESOLUTION
     TVector2 gaussResolution = FitUtils::evalResolution(m, Dm, s, Ds);
-    TString gaussLine = TString::Format("Resolution (gauss), %% = %f #pm %f", gaussResolution.X(), gaussResolution.Y());
+    TString gaussLine = TString::Format("Res. (gauss), %% = %f #pm %f", gaussResolution.X(), gaussResolution.Y());
     CanvasHelper::addTextToStats(gaussLine.Data(), pad);
+
+    // HISTOGRAM RESOLUTION
+    TVector2 histResolution = FitUtils::evalResolution(hist->GetMean(), hist->GetMeanError(), hist->GetStdDev(), hist->GetStdDevError());
+    TString histLine = TString::Format("Res. (hist), %% = %f #pm %f", histResolution.X(), histResolution.Y());
+    CanvasHelper::addTextToStats(histLine.Data(), pad);
 
     // CRYSTAL BALL RESOLUTION
     // TVector2 cbResolution = FitUtils::getCrystalBallResolution(fBall);
@@ -86,6 +98,10 @@ int plotEdep() {
     // Align statistics box
     TPaveStats *pave = CanvasHelper::getDefaultPaveStats(pad);
     CanvasHelper::setPaveAlignment(pave, kPaveAlignLeft | kPaveAlignTop);
+    // pave->SetFillColorAlpha(EColor::kWhite, 0.5);
+
+    // Increase static counter
+    counter++;
   }
 
   // Add canvas title
